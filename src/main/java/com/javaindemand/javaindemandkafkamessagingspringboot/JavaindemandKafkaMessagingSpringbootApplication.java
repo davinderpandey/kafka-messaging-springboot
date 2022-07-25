243014d3bd59ddd3620af798ddbfaa89302da85f
davinderpandey <davinderpandey5@gmail.com>
2022-07-25 13:12:22 +0530

    made few updates related to backend

---
+++ src/main/java/com/javaindemand/javaindemandkafkamessagingspringboot/JavaindemandKafkaMessagingSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.javaindemand.javaindemandkafkamessagingspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaindemandKafkaMessagingSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaindemandKafkaMessagingSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaindemand/javaindemandkafkamessagingspringboot/JavaindemandKafkaMessagingSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaindemand.javaindemandkafkamessagingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaindemandKafkaMessagingSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
